fc51368a80bea246395a7189825e5978d6e47023
jin10 <365150309@qq.com>
2023-12-29 13:40:13 +0800

    Setting up the first py file.

---
+++ Using Python to Interact with the Operating System/Managing Files with Python/Reading files.py
@@ -0,0 +1,8 @@
+file = open("spider.txt")
+print(file.readline())  # 阅读第一行
+print(file.readline())  # 阅读第二行
+print(file.read())  # 阅读剩余部分
+file.close()  # 关闭文件
+
+with open("spider.txt") as file:
+    print(file.readline())  # 使用with语句打开文件spider.txt的行处于读模式。as关键字将file对象赋给变量file。with语句中的代码块将被执行，然后文件将自动关闭。
