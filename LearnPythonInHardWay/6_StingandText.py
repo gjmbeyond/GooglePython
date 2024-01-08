e2bc7f857f9e12088ec29de98dbf638253ebab82
gjmbeyond <365150309@qq.com>
2024-01-08 23:08:23 +0800

    %s 字符串 (采用str()的显示)
    %r 字符串 (采用repr()的显示)
    
    %c 单个字符
    
    %b 二进制整数
    
    %d 十进制整数
    
    %i 十进制整数
    
    %o 八进制整数
    
    %x 十六进制整数
    
    %e 指数 (基底写为e)
    
    %E 指数 (基底写为E)
    
    %f 浮点数
    
    %F 浮点数，与上相同
    
    %g 指数(e)或浮点数 (根据显示长度)
    
    %G 指数(E)或浮点数 (根据显示长度)

---
+++ LearnPythonInHardWay/6_StingandText.py
@@ -0,0 +1,14 @@
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+print(x)
+print(y)
+print("I said: %r." % x)  # 再次输出X变量
+print("I also said: '%s'." % y)  # 再次输出Y变量
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"  # %r直接输出变量内容False而不是布尔值
+print(joke_evaluation % hilarious)  # ("Isn't that joke so funny?! %r" % hilarious)
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w + e)
