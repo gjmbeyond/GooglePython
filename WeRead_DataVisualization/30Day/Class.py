83f4fa40edb9dcde41a5edfef94ff2efb24a44bf
gjmbeyond <365150309@qq.com>
2024-03-27 21:24:15 +0800

    类的初探

---
+++ WeRead_DataVisualization/30Day/Class.py
@@ -0,0 +1,15 @@
+class Phone:
+    boss = '雷军'
+    name = '小米'
+    type = '直板'
+    price = 1999
+
+    def answer(self):
+        print('打电话')
+
+Phone1 = Phone()
+
+print(Phone.boss)
+Phone1.answer()
+
+
